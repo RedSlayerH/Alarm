5d61cfeedfa1598a588cb68feb8b2c11a2d6ea48
RedSlayerH <ron.orman7@gmail.com>
2026-04-09 14:23:20 +0300

    changed url

--- client/js/config.client.js
+++ client/js/config.client.js
@@ -5,4 +5,4 @@
 
 const API_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:3000'
-    : 'https://alarm-production-663a.up.railway.app';
+    : 'https://alarm-production-158b.up.railway.app';
